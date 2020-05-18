b4c40096aefeb1623282fe5e3fecb20f8a657d1d
Masha Rikhter <masha.rikhter@srp-ok.com>
2020-05-18 10:20:37 -0500

    Add long description for promote task
    Co-authored-by: Masha Rikhter <masha.rikhter@srp-ok.com>
    Co-authored-by: Grant Hutchins <grant.hutchins@srp-ok.com>
    Co-authored-by: Adam Milligan <adam@srp-ok.com>

--- lib/stairstep/cli.rb
+++ lib/stairstep/cli.rb
@@ -16,6 +16,16 @@ class Stairstep::CLI < Thor
   class_option("--debug", type: :boolean, desc: "Print out debugging information")
 
   desc "promote ENVIRONMENT", "promote to the given Heroku environment"
+  long_desc <<~DESC
+    You must run this script from within the git repository you plan to deploy from.
+    If you do not specify a deploy commit it will try to deploy HEAD.
+
+    You must have installed the heroku executable and authorized with Heroku.
+    Run `heroku login` if you have not yet authorized with Heroku.
+
+    This script will, by default, generate an annotated tag for the deploy,
+    and push the tag to the `origin` remote.  You may disable this behavior.
+  DESC
   method_option "--from", desc: "Environment to promote from (defaults to the conventional previous environment (e.g. staging <= demo))"
   method_option "--db-capture", type: :boolean, desc: "Capture a snapshot of the database before deploying"
   method_option "--downtime", type: :boolean, default: true, desc: "Bring down the site during deploy (NB: Only skip this for deploys with no migrations)"
